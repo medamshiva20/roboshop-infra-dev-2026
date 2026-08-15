4d775aea1b2d4beb12fef1bc58aa4b7781ba2425
medamshiva20 <medamshiva20@gmail.com>
2026-08-15 16:42:05 +0530

    creating mysql

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -8,7 +8,6 @@ cd /home/ec2-user/
 
 git clone https://github.com/medamshiva20/ansible-roboshop-roles-tf-2026.git
 
-sudo chown -R ec2-user:ec2-user /home/ec2-user/ansible-roboshop-roles-tf-2026
 
 cd ansible-roboshop-roles-tf-2026
 
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -71,3 +71,42 @@ resource "aws_instance" "redis"{
         ]
     }
  }
+
+ resource "aws_instance" "mysql"{
+    ami = local.ami_id
+    instance_type = var.instance_type
+    subnet_id = local.database_subnet_id
+    vpc_security_group_ids = local.mysql_sg_id
+
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-mysql"
+        }
+    )
+ }
+
+ resource "terraform_data" "mysql_bootstrap"{
+    triggers_replace = [
+        aws_instance.mysql.id
+    ]
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.mysql.private_ip
+    }
+
+    provisioner "file"{
+        source = "bootstrap.sh"
+        destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec"{
+        inline = [
+            "sudo chmod +x /tmp/bootstrap.sh",
+            "sudo sh /tmp/bootstrap.sh mysql"
+        ]
+    }
+ }
