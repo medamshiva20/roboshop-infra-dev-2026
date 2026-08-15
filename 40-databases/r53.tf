7c458a00c7caee1b0f79959eabf14870b6e34c15
medamshiva20 <medamshiva20@gmail.com>
2026-08-15 17:03:04 +0530

    creating mysql security group

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -16,7 +16,7 @@ resource "aws_route53_record" "redis"{
 
 resource "aws_route53_record" "mysql"{
     zone_id = var.zone_id
-    name = mysql.${var.domain_name}
+    name = "mysql.${var.domain_name}"
     ttl = "1"
     type = "A"
     records = [aws_instance.mysql.private_ip]
