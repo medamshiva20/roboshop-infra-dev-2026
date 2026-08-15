029c70d0aa74655ace113d1bd6436f4022031f9a
medamshiva20 <medamshiva20@gmail.com>
2026-08-15 14:43:49 +0530

    added redis security group rule

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -5,3 +5,11 @@ resource "aws_route53_record" "mongodb"{
     ttl = "1"
     records = [aws_instance.mongodb.private_ip]
 }
+
+resource "aws_route53_record" "redis"{
+    zone_id = var.zone_id
+    type = "A"
+    ttl = "1"
+    name = redis.${var.domain_name}
+    records = [aws_instance.redis.private_ip]
+}
