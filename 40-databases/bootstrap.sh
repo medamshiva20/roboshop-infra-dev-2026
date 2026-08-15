367001c9dd9cb7e90b9e08e47bba8ed4e65dc7f8
medamshiva20 <medamshiva20@gmail.com>
2026-08-15 15:08:57 +0530

    chnaged the redis host

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,5 +9,5 @@ cd /home/ec2-user/
 git clone https://github.com/medamshiva20/ansible-roboshop-roles-tf-2026.git
 
 cd ansible-roboshop-roles-tf-2026
-
+sudo chown -R ec2-user:ec2-user /home/ec2-user/ansible-roboshop-roles-tf-2026
 ansible-playbook -e component=$component roboshop.yaml
