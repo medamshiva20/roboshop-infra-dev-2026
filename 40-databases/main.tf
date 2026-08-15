b9610aea76bdfc0ee537080e20e5c4367eee965b
medamshiva20 <medamshiva20@gmail.com>
2026-08-15 14:29:57 +0530

    added redis security group rule

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -36,3 +36,36 @@ resource "terraform_data" "bootstrap"{
         ]
     }
 }
+
+resource "aws_instance" "redis"{
+    ami = local.ami_id
+    instance_type = var.instance_type
+    subnet_id = local.database_subnet_id
+    vpc_security_group_ids = [local.redis_sg_id]
+
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-redis"
+        }
+    )
+}
+    resource "terraform_data" "redis_bootstrap"{
+        triggers_replace = [
+            aws_instance.redis.id
+        ]
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.redis.private_ip
+    }
+    provisioner "file"{
+        source = "bootstrap.sh"
+        destination = "/tmp/bootstrap.sh"
+    }
+    provisioner "remote-exec"{
+        "sudo chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis"
+    }
+ }
