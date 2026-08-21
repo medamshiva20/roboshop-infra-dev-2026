c55db311676be1130785b8aec982cf3f7f520deb
medamshiva20 <medamshiva20@gmail.com>
2026-08-21 19:14:33 +0530

    chnaged mongodb r53 record

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mongodb"{
     zone_id = var.zone_id
-    name = "mongodb-dev.${var.domain_name}"
+    name = "mongodb-${var.environment}.${var.domain_name}"
     type = "A"
     ttl = "1"
     records = [aws_instance.mongodb.private_ip]
