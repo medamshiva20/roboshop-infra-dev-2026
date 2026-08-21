dab59c9570a5a569d9707aba66acde02459edb38
medamshiva20 <medamshiva20@gmail.com>
2026-08-21 18:07:31 +0530

    added backend sg id

--- 20-sg_rules/main.tf
+++ 20-sg_rules/main.tf
@@ -62,11 +62,29 @@ resource "aws_security_group_rule" "rabbitmq_bastion"{
     security_group_id = local.rabbitmq_sg_id
 }
 
-resource "aws_security_group_rule" "backend-alb_bation"{
+resource "aws_security_group_rule" "backend_alb_bation"{
     type = "ingress"
     from_port = 80
     to_port = 80
     protocol = "tcp"
     source_security_group_id = local.bastion_sg_id
     security_group_id = local.backend_alb_sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_bastion"{
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    source_security_group_id = local.bastion_sg_id
+    security_group_id = catalogue_sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_backend_alb"{
+    type = "ingress"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    source_security_group_id = backend_alb_sg_id
+    security_group_id = catalogue_sg_id
 }
