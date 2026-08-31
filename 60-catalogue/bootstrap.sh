347d28b580e32be2677cd37296d94f8d54a9f532
medamshiva20 <medamshiva20@gmail.com>
2026-08-31 15:22:59 +0530

    Creating catalogue instance

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -5,7 +5,10 @@ component=$1
 environment=$2
 
 cd /home/ec2-user
-git clone git clone https://github.com/medamshiva20/ansible-roboshop-roles-tf-2026.git
+
+git clone https://github.com/medamshiva20/ansible-roboshop-roles-tf-2026.git
+
 cd ansible-roboshop-roles-tf-2026
 git pull
+
 ansible-playbook -i inventory -e component=$component -e env=$environment roboshop.yaml
