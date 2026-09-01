bd20b0d3466dc519a3dec161e2714ebb1e69516e
medamshiva20 <medamshiva20@gmail.com>
2026-09-01 22:59:01 +0530

    Creating laumch template

--- 50-backend_alb/main.tf
+++ 50-backend_alb/main.tf
@@ -26,7 +26,7 @@ resource "aws_lb_listener" "http"{
 
         fixed_response {
             content_type = "text/html"
-            message_body = "<h1>Hi, I am from Backend HTTP ALB"
+            message_body = "<h1>Hi, I am from Backend HTTP ALB</h1>"
             status_code = "200"
         }
     }
