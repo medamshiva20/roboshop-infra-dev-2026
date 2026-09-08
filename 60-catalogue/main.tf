d41c5508e6ae6a05f47a84805f5ad9535f7f0e28
medamshiva20 <medamshiva20@gmail.com>
2026-09-08 18:32:34 +0530

    added auto scaling group

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -117,3 +117,46 @@ resource "aws_launch_template" "catalogue" {
         local.common_tags
     )
 }
+
+resource "aws_autoscaling_group" "catalogue"{
+    name = "${var.project}-${var.environment}-catalogue"
+    desired_capacity = 1
+    min_size = 1
+    max_size = 10
+    health_check_grace_period = 120
+    health_check_type = "ELB"
+    force_delete = false
+
+    launch_template {
+        id = aws_launch_template.catalogue.id
+        version = "$Latest"
+    }
+
+    vpc_zone_identifier = [local.private_subnet_id]
+    target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+    instance_refresh {
+        strategy = "Rolling"
+        preferences {
+            min_healthy_percentage = 50
+        }
+        triggers = ["launch_template"]
+    }
+    dynamic "tag"{
+        for_each = merge(
+            local.common_tags,
+            {
+                Name = "${var.project}-${var.environment}-catalogue"
+            }
+        )
+        content {
+            key = tag.key
+            value = tag.value
+            propagate_at_launch = true
+        }
+    }
+    # with in 15min autoscaling should be successful
+     timeouts{
+       delete = "15m"
+      }
+}
